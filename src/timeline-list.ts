76a3f68cc477c77407c88a345c5475b48b0fae75
Onukwuf <onukwuf@gmail.com>
2026-03-10 18:52:23 +0100

    feat: implement parseListMembers in timeline-list.ts (#30)

--- src/timeline-list.ts
+++ src/timeline-list.ts
@@ -1,6 +1,6 @@
 import { QueryTweetsResponse } from './timeline-v1';
 import { parseAndPush, TimelineEntryRaw } from './timeline-v2';
-import { Profile } from './profile';
+import { Profile, parseProfile } from './profile';
 import { Tweet } from './tweets';
 
 export interface ListMembersTimeline {
@@ -23,6 +23,41 @@ export interface ListMembersResponse {
   users: Profile[];
   next?: string;
   previous?: string;
+}
+
+export function parseListMembers(
+  timeline: ListMembersTimeline,
+): ListMembersResponse {
+  let bottomCursor: string | undefined;
+  let topCursor: string | undefined;
+  const users: Profile[] = [];
+  const instructions =
+    timeline.data?.list?.members_timeline?.timeline?.instructions ?? [];
+  for (const instruction of instructions) {
+    const entries = instruction.entries ?? [];
+
+    for (const entry of entries) {
+      const entryContent = entry.content;
+      if (!entryContent) continue;
+
+      if (entryContent.cursorType === 'Bottom') {
+        bottomCursor = entryContent.value;
+        continue;
+      } else if (entryContent.cursorType === 'Top') {
+        topCursor = entryContent.value;
+        continue;
+      }
+
+      if (entryContent.itemContent?.user_results?.result) {
+        const result = entryContent.itemContent.user_results.result;
+        if (result.legacy) {
+          users.push(parseProfile(result.legacy, result.is_blue_verified));
+        }
+      }
+    }
+  }
+
+  return { users, next: bottomCursor, previous: topCursor };
 }
 
 export interface ListTimeline {
